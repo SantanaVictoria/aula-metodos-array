41eb5e43d39b0ab69145029d3c82fcf194f6db3d
Victoria Sant'Ana <victoria.santana@confitec.com.br>
2023-06-13 13:33:25 -0300

    bug resolvido

--- alura_books-projeto/app/metodoFilter.js
+++ alura_books-projeto/app/metodoFilter.js
@@ -1,26 +1,30 @@
 const botoes = document.querySelectorAll('.btn')
-botoes.forEach(btn => {
-    btn.addEventListener('click', filtrarLivros)
-})
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-
-    let livrosFiltrados = categoria == 'disponivel' 
-    ? livros.filter(livro => livro.quantidade > 0) 
-    : livros.filter(livro => livro
-    .categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() :
+    filtrarPorCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
     if(categoria == 'disponivel'){
-        exibirValorTotalDosLivrosDisponiveisNaTela()
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal)
     }
 }
 
-function exibirValorTotalDosLivrosDisponiveisNaTela() {
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade(){
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
     elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
-        <div class="livros__disponiveis">
-            <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
-        </div>
+    <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
     `
 }
